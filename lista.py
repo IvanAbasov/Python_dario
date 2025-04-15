0cf73b3effaedeb58727f6974e6c3175f6eff78b
Ivan Abasov <ivan.abasov.t0126450@edu.atec.pt>
2025-04-15 14:24:12 +0100

    listas

---
+++ lista.py
@@ -0,0 +1,10 @@
+noms =  []
+para = True
+
+
+while para :
+    dec = input("Quer continuar ou n?: ")
+    if dec == "s" or dec == "S":
+        break
+
+    noms.append(input("Introduz novo nome: "))
